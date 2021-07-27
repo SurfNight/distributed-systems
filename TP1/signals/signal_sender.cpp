4568ac7989f74605f24f56f4d37e9d6072d1d588
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2021-07-27 19:52:44 -0300

    Add TP1 Signals code

---
+++ TP1/signals/signal_sender.cpp
@@ -0,0 +1,24 @@
+#include <sys/signalfd.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <iostream>
+#include <errno.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+  int pid, signal;
+  cout << "Enter pid and signal" << endl;
+  cin >> pid >> signal;
+  if(kill(pid,signal) == -1){
+    if(errno==ESRCH){
+      cout << "PID inexistente" << endl;
+    }
+    else {
+      cout << "Ocorreu um erro" << endl;
+    }
+    return 1;
+}
+  return 0;
+}
