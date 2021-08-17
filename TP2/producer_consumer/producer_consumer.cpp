0b2a416714abfb20029b4c630114238bd19de091
guilherme bergman <bergmanguilherme2k@gmail.com>
2021-08-17 12:27:56 -0300

    simplificando funções

--- TP2/producer_consumer/producer_consumer.cpp
+++ TP2/producer_consumer/producer_consumer.cpp
@@ -24,31 +24,10 @@ int generateRandomNumber() {
     return (rand() % 10000000) + 1;
 }
 
-void createThread(void (*function) (void)) {
-    // function that creates a thread given a function pointer
-    thread t(function);
-}
-
-int* createZeroedArray(int size)
-{
-    // function that creates a zeroed array of size "size"
-    int *array = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = 0;
-    }
-    return array;
-}
-
-void fullizeSemaphore()
-{
-    // function that sets the semaphore "full" to its maximum size
-    sem_init(&full,1,memory_size);
-}
 
 void addResource(int number)
 {   
-    // function that receiver a number and stores it at the shared memory
+    // function that receives a number and stores it at the shared memory
     for (int i = 0; i < memory_size; i++) {
         // find a free space of the memory to store the number
         if (memory[i] == 0) {
@@ -107,14 +86,14 @@ void consumer()
 void createProducerThreads(int num_threads) {
     // create num_threads threads that will produce numbers
     for (int i = 0; i < num_threads; i++) {
-        createThread(&producer);
+        thread t(&producer);//creates a thread given a function pointer
     }
 }
 
 void createConsumerThreads(int num_threads) {
     // create num_threads threads that will consume numbers
     for (int i = 0; i < num_threads; i++) {
-        createThread(&consumer);
+        thread t(&consumer);//creates a thread given a function pointer
     }
 }
 
@@ -124,8 +103,8 @@ int main()
     int Np, Nc;
     cout << "Insira o tamanho da memória, o número de produtores e o número de consumidores" << endl;
     cin >> memory_size >> Np >> Nc;
-    memory = createZeroedArray(memory_size);
-    fullizeSemaphore();
+    int *memory = new int[memory_size]{0};//creates a zeroed array of size "memory_size"
+    sem_init(&full,1,memory_size);//sets the semaphore "full" to its maximum size
     // creating threads
     createConsumerThreads(Nc);
     createProducerThreads(Np);
