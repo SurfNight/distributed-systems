1573dcf7ae93526501a3d07a71aee4f3adf810c6
guilhermebergman <guilhermebergman@poli.ufrj.br>
2021-08-20 15:24:42 -0300

    joining threads

--- TP2/producer_consumer/producer_consumer.cpp
+++ TP2/producer_consumer/producer_consumer.cpp
@@ -11,12 +11,17 @@
 #include "../../lib/MyLib.h"
 
 using namespace std;
+
 int memory_size;
 int *memory;
 int consumed;
+bool consumed_all = false;
+
+thread **consumer_threads;
+thread **producer_threads;
 
 sem_t mutex;
-sem_t empty_semaphore;
+sem_t empty;
 sem_t full;
 
 int generateRandomNumber()
@@ -50,7 +55,7 @@ void consume()
         // find a number to consume (number != 0)
         if (number != 0)
         {
-            cout << "CONSUMER: O número " << number << (isPrime(number) ? "" : " não") << " é primo." << endl;
+            cout << "CONSUMER: O número " << number << (isPrime(number) ? "" : " não") << " é primo. " << consumed + 1 << endl;
             // consume number and set its memory location to 0
             memory[i] = 0;
             // add 1 to consumed count
@@ -62,63 +67,84 @@ void consume()
 
 void producer()
 {
-    // generate a resource
-    int number = generateRandomNumber();
-    sem_wait(&empty_semaphore);
-    sem_wait(&mutex);
-    // add resource to shared memory
-    addResource(number);
-    sem_post(&mutex);
-    sem_post(&full);
+    while (!consumed_all)
+    {
+        // generate a resource
+        int number = generateRandomNumber();
+        sem_wait(&empty);
+        sem_wait(&mutex);
+        // add resource to shared memory
+        addResource(number);
+        sem_post(&mutex);
+        sem_post(&full);
+    }
 }
 
 void consumer()
 {
-    int number;
-    sem_wait(&full);
-    sem_wait(&mutex);
-    consume();
-    if (consumed == 10000)
+    while (!consumed_all)
     {
-        exit(0);
+        int number;
+        sem_wait(&full);
+        sem_wait(&mutex);
+        consume();
+        if (consumed == 10000)
+        {
+            consumed_all = true;
+        }
+        sem_post(&mutex);
+        sem_post(&empty);
     }
-    sem_post(&mutex);
-    sem_post(&empty_semaphore);
 }
 
 void createProducerThreads(int num_threads)
 {
+    producer_threads = new thread *[num_threads];
     // create num_threads threads that will produce numbers
     for (int i = 0; i < num_threads; i++)
     {
-        thread t(&producer); //creates a thread given a function pointer
+        producer_threads[i] = new thread(&producer); //creates a thread given a function pointer
     }
 }
 
 void createConsumerThreads(int num_threads)
 {
-    // create num_threads threads that will consume numbers
+    consumer_threads = new thread *[num_threads];
+    // create num_threads threads that will produce numbers
     for (int i = 0; i < num_threads; i++)
     {
-        thread t(&consumer); //creates a thread given a function pointer
+        consumer_threads[i] = new thread(&consumer); //creates a thread given a function pointer
+    }
+}
+
+void joinConsumerThreads(int Nc)
+{
+    for (int i = 0; i < Nc; i++)
+    {
+        consumer_threads[i]->join();
+    }
+}
+void joinProducerThreads(int Np)
+{
+    for (int i = 0; i < Np; i++)
+    {
+        producer_threads[i]->join();
     }
 }
 
 int main()
 {
-    sem_init(&mutex, 1, 1);
-
     srand(time(NULL));
     int Np, Nc;
     cout << "Insira o tamanho da memória, o número de produtores e o número de consumidores" << endl;
     cin >> memory_size >> Np >> Nc;
-    int *memory = new int[memory_size]{0}; //creates a zeroed array of size "memory_size"
-    sem_init(&full, 1, memory_size);       //sets the semaphore "full" to its maximum size
+    memory = new int[memory_size]{0}; //creates a zeroed array of size "memory_size"
+    sem_init(&mutex, 1, 1);
+    sem_init(&full, 1, memory_size); //sets the semaphore "full" to its maximum size
+    sem_init(&empty, 1, 0);
     // creating threads
     createConsumerThreads(Nc);
     createProducerThreads(Np);
-    while (true)
-    {
-        // waiting for 10000 numbers to be consumed
-    }
+    joinConsumerThreads(Nc);
+    joinProducerThreads(Np);
 }
