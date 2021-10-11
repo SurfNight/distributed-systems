27da8309fbd93a7f8ba4dec7078f1989bcaf7097
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2021-10-10 22:22:37 -0300

    fix test_0 and test_1 scripts

--- TP3/test_0.sh
+++ TP3/test_0.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-./cliente 2 10 172.26.86.132 &
-./cliente 2 10 172.26.86.132 &
+./cliente 2 10 127.0.0.1 &
+./cliente 2 10 127.0.0.1 &
 
 wait
+
+cp resultados.txt test_0/resultados.txt
+rm -f resultados.txt
--- TP3/test_1.sh
+++ TP3/test_1.sh
@@ -3,7 +3,11 @@
 
 for i in 4 8 16 32; do
     for j in $(seq $i); do
-        ./cliente 2 10 172.26.86.132 &
+        ./cliente 2 10 127.0.0.1 &
     done
     wait
+    cp resultado.txt test_1/resultados/$i.txt
+     > resultado.txt
 done
+
+rm -f resultado.txt
